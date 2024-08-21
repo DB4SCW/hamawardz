59d57eead8fc9eb06e10f761cfb116839f40a210
DB4SCW <dev@db4scw.de>
2024-08-21 17:46:08 +0200

    change sqlite database handling

--- tests/cypress/integration/example.cy.js
+++ tests/cypress/integration/example.cy.js
@@ -1,7 +1,12 @@
 describe('Example Test', () => {
+
+    before(() => {
+        cy.refreshDatabase();
+    });
+
     it('shows a homepage', () => {
         cy.visit('/');
 
-        cy.contains('Laravel');
+        cy.contains('Hamawardz');
     });
 });
