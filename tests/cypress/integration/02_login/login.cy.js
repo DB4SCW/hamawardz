e69692378c09dacf1abbb369b5116a6ef35fb2f0
DB4SCW <dev@db4scw.de>
2024-08-21 18:43:48 +0200

    Login test

--- tests/cypress/integration/02_login/login.cy.js
+++ tests/cypress/integration/02_login/login.cy.js
@@ -1,12 +1,19 @@
 describe('Login Test', () => {
-
+    
     //refresh database before the whole test suite
-    before(() => {
+    before(function () {
         cy.refreshDatabase();
     });
 
+    //load fixture data
+    beforeEach(function () {
+        cy.fixture('fixed_data').then((fixed_data) => {
+          this.fixed_data = fixed_data
+        })
+    });
+
     //tests if we can switch to the login page
-    it('switches to the login page', () => {
+    it('switches to the login page', function () {
         cy.visit('/');
         cy.get('#loginlink').click();   
         cy.location('pathname').should('eq', '/login');
@@ -14,11 +21,11 @@ describe('Login Test', () => {
     });
 
     //logs the base administrator in and out
-    it('logs the base administrator in and out', () => {
+    it('logs the base administrator in and out', function() {
         cy.visit('/');
         cy.get('#loginlink').click();   
-        cy.get('#username').type('administrator');
-        cy.get('#password').type('welcome#01');
+        cy.get('#username').type(this.fixed_data.admin_username);
+        cy.get('#password').type(this.fixed_data.adminpassword);
         cy.get('.btn').click();
         cy.contains('Login successful. Welcome back!');
         cy.contains('Logout');
